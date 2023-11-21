9f5d8e797521ec57b88c0cef4f542301e14c9cce
palindrom8 <andr.nitgrafika@yandex.ru>
2023-11-21 23:53:55 +0300

    Counting goals

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : NetworkBehaviour
+{
+    [SerializeField] float speed = 30f;
+    [SerializeField] Rigidbody2D rigidbody2d;
+
+
+    public override void OnStartServer()
+    {
+        rigidbody2d.simulated = true;
+        rigidbody2d.velocity = Vector2.right * speed;
+    }
+
+    [ServerCallback]
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.transform.GetComponent<Player>()) return;
+        float x = rigidbody2d.velocity.x > 0 ? -1 : 1;
+        float y = (transform.position.y - collision.transform.position.y) / collision.collider.bounds.size.y;
+        Vector2 direction = new Vector2(x, y).normalized;
+        rigidbody2d.velocity = direction * speed;
+    }
+}
